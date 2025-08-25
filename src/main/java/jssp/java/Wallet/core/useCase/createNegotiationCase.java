263a6809e7023d2ae09f64f155a7583c86b37885
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-08-25 18:49:29 -0300

    negotiationcase

--- src/main/java/jssp/java/Wallet/core/useCase/createNegotiationCase.java
+++ src/main/java/jssp/java/Wallet/core/useCase/createNegotiationCase.java
@@ -1,4 +1,8 @@
 package jssp.java.Wallet.core.useCase;
 
+import jssp.java.Wallet.core.entities.Negotiation;
+
 public interface createNegotiationCase {
+
+    public Negotiation execute(Negotiation negotiation);
 }
