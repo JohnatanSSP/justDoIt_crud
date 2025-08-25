6aa31d4bd41a197b04a48b55e4af87719d8ccd28
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-08-25 18:49:44 -0300

    negotiationcaseimpl

---
+++ src/main/java/jssp/java/Wallet/core/useCase/createNegotiationCaseImpl.java
@@ -0,0 +1,13 @@
+package jssp.java.Wallet.core.useCase;
+
+
+import jssp.java.Wallet.core.entities.Negotiation;
+
+public class createNegotiationCaseImpl implements createNegotiationCase {
+
+    @Override
+    public Negotiation execute(Negotiation negotiation){
+
+        return negotiation;
+    }
+}
